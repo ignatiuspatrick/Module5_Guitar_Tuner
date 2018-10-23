abaa75ba21558ad9ef563ef31305226eb3d58812
jandouwe <l.j.d.beekman@student.utwente.nl>
2018-10-23 16:44:45 +0200

    GUI changes

--- UI/CompileREADME.md
+++ UI/CompileREADME.md
@@ -1,2 +1,3 @@
 sudo apt-get install gtk+2.0
+
 gcc GUIview.c -o GUIview `pkg-config --cflags --libs gtk+-2.0`
--- UI/GUIview.c
+++ UI/GUIview.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <gtk/gtk.h>
 
 float GUIptGetInput(){
@@ -14,11 +15,25 @@ int main( int   argc,
           char *argv[] )
 {
     GtkWidget *window;
+    GtkWidget *label;
+    GtkWidget *entry;
 
     gtk_init (&argc, &argv);
 
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+
+    label = gtk_label_new(NULL);
+    const char *str = "some text";
+    gtk_label_set_text(label, str);
+
+    gtk_container_add(GTK_CONTAINER (window), label);
     gtk_widget_show  (window);
+    gtk_widget_show (label);
+
+    delay(1000);
+    const char *str2 = "some different text";
+    gtk_label_set_text(label, str2);
+    gtk_widget_show (label);
 
     g_signal_connect (window, "destroy", G_CALLBACK (destroy), NULL);
 
