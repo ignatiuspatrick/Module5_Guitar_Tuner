3f20598f10261a8fa46342c111d243df3c6fe948
jandouwe <l.j.d.beekman@student.utwente.nl>
2018-10-29 10:19:50 +0100

    working buttons

--- UI/GUIview.c
+++ UI/GUIview.c
@@ -5,15 +5,40 @@
 
 GtkWidget *window;
 GtkWidget *displayText;
+GtkWidget *flabel;
 GtkWidget *entry;
-//GtkWidget*[] buttons;
+GtkWidget *buttons[4];
+int buttonNumber = 0;
 
 void setDisplayText(char* str){
     gtk_label_set_text(GTK_LABEL (displayText), str);
 }
 
-void setButton(char* str){
+void setButton(void*, char*);
+void removeButtons();
 
+void nextEvent(){
+    removeButtons();
+    char *but2 = "doe";
+    setButton(nextEvent, but2);
+}
+
+void removeButtons(){
+    for(int i = 0; i < buttonNumber; i++){
+        gtk_widget_destroy(buttons[i]);
+    }
+    buttonNumber = 0;
+}
+
+void setButton(void* function, char* str){
+    GtkWidget *button;
+    button = gtk_button_new_with_label(str);
+    gtk_fixed_put(GTK_FIXED (flabel), button, 200, 100 + buttonNumber*50);
+    g_signal_connect (button, "clicked",
+                      G_CALLBACK (function), NULL);
+    buttons[buttonNumber] = button;
+    buttonNumber++;
+    gtk_widget_show  (button);
 }
 
 float GUIptGetInput(){
@@ -40,20 +65,30 @@ int main( int   argc,
     gtk_window_set_default_size(GTK_WINDOW (window), 500, 750);
 
     displayText = gtk_label_new(NULL);
-    GtkWidget *flabel = gtk_fixed_new();
+    flabel = gtk_fixed_new();
     gtk_container_add(GTK_CONTAINER (window), flabel);
     gtk_fixed_put(GTK_FIXED (flabel), displayText, 100, 50);
 
     gtk_widget_show_all  (window);
 
-    pthread_t gui_thread;
-    pthread_create(&gui_thread, NULL, *runGUI, NULL);
+    //pthread_t gui_thread;
+    //pthread_create(&gui_thread, NULL, *runGUI, NULL);
 
-    char *str2 = "pizza is echt super lekker, waarom weet \nniet iedereen dit dafd adfa efadf eafea ef";
+    char *str2 = "pizza is echt super lekker";
     setDisplayText(str2);
 
-    g_signal_connect (window, "destroy", G_CALLBACK (destroy), NULL);
+    char *but1 = "hoi";
+    setButton(nextEvent, but1);
 
-    pthread_join(gui_thread, NULL);
+    /*for(int i = 0; i <1000000000; i++){
+
+    }
+
+    char *but2 = "doe";
+    setButton(but2);*/
+
+    g_signal_connect (window, "destroy", G_CALLBACK (destroy), NULL);
+    gtk_main();
+    //pthread_join(gui_thread, NULL);
     return 0;
 }
